9453aaeed2bf8df7220cf7574b5fd6d58e23a070
KirillValetko <kirya_valet@mail.ru>
2023-12-10 13:50:31 +0300

    added common files

---
+++ GameHub.Common/Constants/ExceptionMessageConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace GameHub.Common.Constants
+{
+    public class ExceptionMessageConstants
+    {
+        public const string EntityIsNotFound = "Entity is not found";
+    }
+}
---
+++ GameHub.Common/Constants/SwaggerOptionsConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace GameHub.Common.Constants
+{
+    public class SwaggerOptionsConstants
+    {
+        public const string Version = "v1";
+        public const string Title = "GameHub API";
+        public const string Description = "GameHub";
+        public const string SecurityScheme = "Authorization";
+        public const string BearerFormat = "JWT";
+        public const string DefinitionName = "Authorization";
+    }
+}
