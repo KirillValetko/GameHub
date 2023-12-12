0292e6cc278877f982cbe813c915e5ff7dab6e27
KirillValetko <kirya_valet@mail.ru>
2023-12-12 16:30:47 +0300

    added authentication controller

---
+++ GameHub.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using GameHub.BLL.Models;
+using GameHub.BLL.Services.Interfaces;
+using GameHub.Web.Models.DtoModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameHub.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : BaseController
+    {
+        private readonly IAuthenticationService _authenticationService;
+        
+        public AuthenticationController(IAuthenticationService authenticationService,
+            IMapper mapper,
+            ILogger<AuthenticationController> logger) : base(mapper, logger)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public Task<IActionResult> PostAsync(LoginDto item)
+        {
+            var mappedItem = _mapper.Map<LoginModel>(item);
+
+            return ProcessRequestAsync<string>(() => _authenticationService.AuthenticateAsync(mappedItem));
+        }
+    }
+}
---
+++ GameHub.Web/Models/DtoModels/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GameHub.Web.Models.DtoModels
+{
+    public class LoginDto
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
