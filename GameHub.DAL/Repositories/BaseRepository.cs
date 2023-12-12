bbf435e00c34a18f9f563fa4ec2e567244acedad
KirillValetko <kirya_valet@mail.ru>
2023-12-12 11:50:14 +0300

    changed DAL base behaviour

--- GameHub.DAL/Repositories/BaseRepository.cs
+++ GameHub.DAL/Repositories/BaseRepository.cs
@@ -2,10 +2,13 @@
 using GameHub.Common.Constants;
 using GameHub.DAL.DataModels;
 using GameHub.DAL.Filters;
+using GameHub.DAL.Infrastructure.DbSettings;
 using GameHub.DAL.Models;
 using GameHub.DAL.Repositories.Interfaces;
+using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace GameHub.DAL.Repositories
 {
@@ -18,11 +21,12 @@ namespace GameHub.DAL.Repositories
         protected readonly IMongoCollection<TDbModel> _collection;
         protected readonly IMapper _mapper;
 
-        protected BaseRepository(IMapper mapper)
+        protected BaseRepository(IMapper mapper, 
+            IOptions<BaseGameHubDbSettings<TDbModel>> settings)
         {
-            var mongoClient = new MongoClient();
-            var mongoDatabase = mongoClient.GetDatabase("");
-            _collection = mongoDatabase.GetCollection<TDbModel>("");
+            var mongoClient = new MongoClient(settings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(settings.Value.DatabaseName);
+            _collection = mongoDatabase.GetCollection<TDbModel>(settings.Value.CollectionName);
             _mapper = mapper;
         }
 
@@ -41,8 +45,8 @@ namespace GameHub.DAL.Repositories
                 throw new Exception(ExceptionMessageConstants.EntityIsNotFound);
             }
 
-            var mappedItem = _mapper.Map<TDbModel>(item);
-            await _collection.ReplaceOneAsync(i => i.Id == item.Id, mappedItem);
+            _mapper.Map(item, dbItem);
+            await _collection.ReplaceOneAsync(i => i.Id == item.Id, dbItem);
         }
 
         public virtual async Task DeleteAsync(string id)
@@ -59,9 +63,9 @@ namespace GameHub.DAL.Repositories
 
         public async Task<TDataModel> GetByFilterAsync(TFilter filter)
         {
-            var bsonFilter = ConstructFilter(filter);
+            var source = ConstructFilter(filter);
 
-            var item = await _collection.Find(bsonFilter).FirstOrDefaultAsync();
+            var item = await source.FirstOrDefaultAsync();
             var mappedItem = _mapper.Map<TDataModel>(item);
 
             return mappedItem;
@@ -69,29 +73,29 @@ namespace GameHub.DAL.Repositories
 
         public async Task<List<TDataModel>> GetAllByFilterAsync(TFilter filter)
         {
-            var bsonFilter = ConstructFilter(filter);
+            var source = ConstructFilter(filter);
 
-            var items = await _collection.FindAsync(bsonFilter);
+            var items = await source.ToListAsync();
             var mappedItems = _mapper.Map<List<TDataModel>>(items);
             
             return mappedItems;
         }
 
-        private BsonDocument ConstructFilter(TFilter filter)
+        private IMongoQueryable<TDbModel> ConstructFilter(TFilter filter)
         {
-            var bsonFilter = new BsonDocument();
+            var source = _collection.AsQueryable();
             filter ??= new TFilter();
 
             if (!string.IsNullOrEmpty(filter.Id))
             {
-                bsonFilter.Add("_id", filter.Id);
+                source = source.Where(i => i.Id.Equals(filter.Id));
             }
 
-            bsonFilter = AddFilterConditions(bsonFilter, filter);
+            source = AddFilterConditions(source, filter);
 
-            return bsonFilter;
+            return source;
         }
 
-        protected abstract BsonDocument AddFilterConditions(BsonDocument bsonFilter, TFilter filter);
+        protected abstract IMongoQueryable<TDbModel> AddFilterConditions(IMongoQueryable<TDbModel> source, TFilter filter);
     }
 }
