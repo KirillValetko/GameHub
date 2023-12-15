de6d256ae298cd67a4991fe3d13e1c6e491e391d
KirillValetko <kirya_valet@mail.ru>
2023-12-15 11:56:38 +0300

    added game difficulty controller

---
+++ GameHub.Web/Controllers/GameDifficultyController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using GameHub.BLL.Models;
+using GameHub.BLL.Services.Interfaces;
+using GameHub.Common.Constants;
+using GameHub.DAL.Filters;
+using GameHub.Web.Infrastructure.Attributes;
+using GameHub.Web.Models.DtoModels;
+using GameHub.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameHub.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class GameDifficultyController : BaseController
+    {
+        private readonly IGameDifficultyService _gameDifficultyService;
+
+        public GameDifficultyController(IGameDifficultyService gameDifficultyService,
+            IMapper mapper,
+            ILogger<BaseController> logger) : base(mapper, logger)
+        {
+            _gameDifficultyService = gameDifficultyService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public Task<IActionResult> GetAsync([FromQuery] GameDifficultyFilter filter)
+        {
+            return ProcessRequestAsync<List<GameDifficultyModel>, List<GameDifficultyViewModel>>(() =>
+                _gameDifficultyService.GetAllByFilterAsync(filter));
+        }
+
+        [HttpPost]
+        [RequiresRoleClaim(RoleConstants.Admin)]
+        public Task<IActionResult> PostAsync(GameDifficultyDto item)
+        {
+            var mappedItem = _mapper.Map<GameDifficultyModel>(item);
+
+            return ProcessRequestAsync<GameDifficultyViewModel>(() =>
+                _gameDifficultyService.CreateAsync(mappedItem));
+        }
+
+        [HttpPut]
+        [RequiresRoleClaim(RoleConstants.Admin)]
+        public Task<IActionResult> PutAsync(GameDifficultyDto item)
+        {
+            var mappedItem = _mapper.Map<GameDifficultyModel>(item);
+
+            return ProcessRequestAsync<GameDifficultyViewModel>(() =>
+                _gameDifficultyService.UpdateAsync(mappedItem));
+        }
+
+        //[HttpDelete]
+        //[RequiresRoleClaim(RoleConstants.Admin)]
+        //public Task<IActionResult> DeleteAsync(string id)
+        //{
+        //    return ProcessRequestAsync<GameDifficultyViewModel>(() =>
+        //        _gameDifficultyService.DeleteAsync(id));
+        //}
+    }
+}
---
+++ GameHub.Web/Models/ViewModels/GameDifficultyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace GameHub.Web.Models.ViewModels
+{
+    public class GameDifficultyViewModel : BaseViewModel
+    {
+        public string DifficultyName { get; set; }
+        public int DifficultyValue { get; set; }
+        public string GameId { get; set; }
+        public Object DifficultyParameters { get; set; }
+    }
+}
