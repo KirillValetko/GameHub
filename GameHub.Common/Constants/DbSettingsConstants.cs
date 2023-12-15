a34dd1502be8204af1c535806015215491ead544
KirillValetko <kirya_valet@mail.ru>
2023-12-15 22:37:47 +0300

    added user game stats repository

--- GameHub.Common/Constants/DbSettingsConstants.cs
+++ GameHub.Common/Constants/DbSettingsConstants.cs
@@ -7,5 +7,6 @@
         public const string UsersCollection = "GameHubDB:UsersCollection";
         public const string GamesCollection = "GameHubDB:GamesCollection";
         public const string GameDifficultiesCollection = "GameHubDB:GameDifficultiesCollection";
+        public const string UserGameStatsCollection = "GameHubDB:UserGameStatsCollection";
     }
 }
