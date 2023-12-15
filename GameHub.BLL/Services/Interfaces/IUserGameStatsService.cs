ca4ccf366db2f86d0ea6bf12b939c2d262e03083
KirillValetko <kirya_valet@mail.ru>
2023-12-15 22:38:16 +0300

    added user game stats service

---
+++ GameHub.BLL/Services/Interfaces/IUserGameStatsService.cs
@@ -0,0 +1,11 @@
+﻿using GameHub.BLL.Models;
+using GameHub.DAL.DataModels;
+using GameHub.DAL.Filters;
+using GameHub.DAL.Models;
+
+namespace GameHub.BLL.Services.Interfaces
+{
+    public interface IUserGameStatsService : IBaseService<UserGameStats, UserGameStatsDataModel, UserGameStatsModel, UserGameStatsFilter>
+    {
+    }
+}
