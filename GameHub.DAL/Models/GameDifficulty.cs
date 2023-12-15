6ca8e4f25c0a18fb1f9a75c0b8f38adc5cc978e1
KirillValetko <kirya_valet@mail.ru>
2023-12-15 11:54:23 +0300

    added game difficulty repository

---
+++ GameHub.DAL/Models/GameDifficulty.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using MongoDB.Bson;
+
+namespace GameHub.DAL.Models
+{
+    public class GameDifficulty : BaseDbModel
+    {
+        public string DifficultyName { get; set; }
+        public int DifficultyValue { get; set; }
+        public string GameId { get; set; }
+        public BsonDocument DifficultyParameters { get; set; }
+    }
+}
---
+++ GameHub.DAL/Repositories/Interfaces/IGameDifficultyRepository.cs
@@ -0,0 +1,10 @@
+﻿using GameHub.DAL.DataModels;
+using GameHub.DAL.Filters;
+using GameHub.DAL.Models;
+
+namespace GameHub.DAL.Repositories.Interfaces
+{
+    public interface IGameDifficultyRepository : IBaseRepository<GameDifficulty, GameDifficultyDataModel, GameDifficultyFilter>
+    {
+    }
+}
