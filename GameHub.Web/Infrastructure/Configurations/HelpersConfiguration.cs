e5eec25abeb81af05f3608d7192402c3a432cce8
KirillValetko <kirya_valet@mail.ru>
2023-12-16 15:36:37 +0300

    added mapper profiles and di

---
+++ GameHub.Web/Infrastructure/Configurations/HelpersConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using GameHub.Common.Helpers;
+using GameHub.Common.Helpers.Interfaces;
+using GameHub.DAL.Models;
+
+namespace GameHub.Web.Infrastructure.Configurations
+{
+    public static class HelpersConfiguration
+    {
+        public static void InitHelpers(this IServiceCollection services)
+        {
+            services.AddScoped<IPaginationHelper<User>, PaginationHelper<User>>();
+            services.AddScoped<IPaginationHelper<Game>, PaginationHelper<Game>>();
+            services.AddScoped<IPaginationHelper<GameDifficulty>, PaginationHelper<GameDifficulty>>();
+            services.AddScoped<IPaginationHelper<UserGameStats>, PaginationHelper<UserGameStats>>();
+        }
+    }
+}
