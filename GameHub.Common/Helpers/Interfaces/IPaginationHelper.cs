fc4b6f321f3b4244e39155509c81ec8e38822fe4
KirillValetko <kirya_valet@mail.ru>
2023-12-16 15:33:59 +0300

    added pagination helper

---
+++ GameHub.Common/Helpers/Interfaces/IPaginationHelper.cs
@@ -0,0 +1,11 @@
+﻿using GameHub.Common.Models;
+using MongoDB.Driver.Linq;
+
+namespace GameHub.Common.Helpers.Interfaces
+{
+    public interface IPaginationHelper<T>
+        where T : class
+    {
+        Task<PaginationResponse<T>> PaginateAsync(IMongoQueryable<T> source, int? pageNumber, int? limit);
+    }
+}
---
+++ GameHub.Common/Models/PaginationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace GameHub.Common.Models
+{
+    public class PaginationRequest<T>
+        where T : class
+    {
+        public int? PageNumber { get; set; }
+        public int? Limit { get; set; }
+        public T Filter { get; set; }
+    }
+}
